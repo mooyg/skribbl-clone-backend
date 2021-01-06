bf46861430713b020a49c8a3f61a8f81589e3f0f
mooyg <mooybot@gmail.com>
2021-01-06 18:43:47 +0530

    first

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const app = express()
+const socket = require('socket.io')
+const cors = require('cors')
+
+app.get('/', (req,res) =>{
+    res.send("HELLO WORLD")
+})
+
+let server = app.listen('8000', () => {
+    console.log("App on")
+})
+let io = socket(server, {
+    // Now, the CORS config.
+    // You could either use the new `cors` property...
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+        allowedHeaders: ["content-type"]
+    },
+
+})
+let userList = []
+io.on('connection', (socket)=>{
+    console.log('connection made', socket.id)
+    userList.push(socket.id)
+    io.emit('new-connection', userList)
+    socket.on('disconnect', (socket)=>{
+        console.log('dc')
+        let index = userList.indexOf(socket.id)
+        userList.splice(index, 1)
+    })
+    socket.on('on-draw', (data)=>{
+        console.log(data)
+        if(userList[0] === socket.id){
+            socket.broadcast.emit("broadcast", data);
+        }
+    })
+    socket.on('refresh-userList', (data)=>{
+        console.log('data')
+        userList = data
+    })
+})
+
+
